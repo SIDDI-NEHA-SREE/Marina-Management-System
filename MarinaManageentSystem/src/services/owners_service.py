b7171a256f5100b341e51885b4ebfd3bed271789
SIDDI-NEHA-SREE <siddinehasree@gmail.com>
2025-10-01 16:42:46 +0530

    Update owners_service.py

--- MarinaManageentSystem/src/services/owners_service.py
+++ MarinaManageentSystem/src/services/owners_service.py
@@ -1,28 +1,17 @@
-from src.utils.supabase_client import supabase
-from src.models.owner import Owner
+from src.dao.base_dao import BaseDAO
 
 class OwnersService:
     def __init__(self):
-        self.client = supabase
-        self.table = "mmsowners"
+        self.dao = BaseDAO("mmsowners")
 
-    def create_owner(self, owner: Owner):
-        """Insert a new owner into the database"""
-        res = self.client.table(self.table).insert(owner.to_dict()).execute()
-        return res.data
+    def create_owner(self, owner):
+        return self.dao.insert(owner.to_dict())
 
     def list_owners(self):
-        """Fetch all owners"""
-        res = self.client.table(self.table).select("*").execute()
-        return res.data
+        return self.dao.get_all()
 
-    def update_owner(self, owner_id: int, updated_data: dict):
-        """Update owner details"""
-        res = self.client.table(self.table).update(updated_data).eq("owner_id", owner_id).execute()
-        return res.data
+    def update_owner(self, owner_id, data):
+        return self.dao.update(owner_id, data, id_field="owner_id")
 
-    def delete_owner(self, owner_id: int):
-        """Delete owner by ID"""
-        res = self.client.table(self.table).delete().eq("owner_id", owner_id).execute()
-        return res.data
-
+    def delete_owner(self, owner_id):
+        return self.dao.delete(owner_id, id_field="owner_id")
