04e905078b1355c1d0bf22ee24a76ca662f7bc7e
SIDDI-NEHA-SREE <siddinehasree@gmail.com>
2025-10-01 16:05:28 +0530

    Update vessels_service.py

--- MarinaManageentSystem/src/services/vessels_service.py
+++ MarinaManageentSystem/src/services/vessels_service.py
@@ -1,12 +1,18 @@
-from src.dao.vessels_dao import VesselsDAO
+from src.dao.base_dao import BaseDAO
 from src.models.vessel import Vessel
 
 class VesselsService:
     def __init__(self):
-        self.dao = VesselsDAO()
+        self.dao = BaseDAO("mmsvessels", "vessel_id")
 
     def create_vessel(self, vessel: Vessel):
         return self.dao.insert(vessel.to_dict())
 
     def list_vessels(self):
         return self.dao.select().data
+
+    def update_vessel(self, vessel_id, updated_data):
+        return self.dao.update(vessel_id, updated_data)
+
+    def delete_vessel(self, vessel_id):
+        return self.dao.delete(vessel_id)
