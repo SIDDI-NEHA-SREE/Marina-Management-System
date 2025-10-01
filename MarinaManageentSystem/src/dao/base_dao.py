2a187c8b125449fcfc64f41c178b1ade03690194
SIDDI-NEHA-SREE <siddinehasree@gmail.com>
2025-10-01 15:56:15 +0530

    Update base_dao.py

--- MarinaManageentSystem/src/dao/base_dao.py
+++ MarinaManageentSystem/src/dao/base_dao.py
@@ -1,32 +1,20 @@
+from src.utils.supabase_client import supabase
+
 class BaseDAO:
-    def __init__(self, client, table):
-        self.client = client
-        self.table = table
+    def __init__(self, table_name, id_field=None):
+        # Force lowercase table names to match Supabase/Postgres
+        self.table = table_name.lower()
+        self.client = supabase
+        self.id_field = id_field or f"{self.table}_id"
 
-    def insert(self, data):
-        return self.client.table(self.table).insert(data).execute().data
+    def insert(self, data: dict):
+        return self.client.table(self.table).insert(data).execute()
 
-    def select(self, filters=None):
-        query = self.client.table(self.table).select("*")
-        if filters:
-            for key, value in filters.items():
-                query = query.eq(key, value)
-        return query.execute().data
+    def select(self):
+        return self.client.table(self.table).select("*").execute()
 
-    def update(self, record_id, data, id_field="id"):
-        return (
-            self.client.table(self.table)
-            .update(data)
-            .eq(id_field, record_id)
-            .execute()
-            .data
-        )
+    def update(self, record_id, data: dict):
+        return self.client.table(self.table).update(data).eq(self.id_field, record_id).execute()
 
-    def delete(self, record_id, id_field="id"):
-        return (
-            self.client.table(self.table)
-            .delete()
-            .eq(id_field, record_id)
-            .execute()
-            .data
-        )
+    def delete(self, record_id):
+        return self.client.table(self.table).delete().eq(self.id_field, record_id).execute()
