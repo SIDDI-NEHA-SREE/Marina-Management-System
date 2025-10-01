daaa6ec7ffa2ba62e48703b30d136b65c134af84
SIDDI-NEHA-SREE <siddinehasree@gmail.com>
2025-10-01 16:41:26 +0530

    Update payment.py

--- MarinaManageentSystem/src/models/payment.py
+++ MarinaManageentSystem/src/models/payment.py
@@ -1,19 +1,18 @@
 from datetime import date
 
 class Payment:
-    def __init__(self, vessel_id, amount, payment_type, tax_amount=0.0, payment_id=None, payment_date=None):
-        self.payment_id = payment_id
+    def __init__(self, vessel_id, amount, payment_type, tax_amount=0.0):
         self.vessel_id = vessel_id
         self.amount = amount
-        self.payment_date = payment_date or date.today()
         self.payment_type = payment_type
         self.tax_amount = tax_amount
+        self.payment_date = date.today()
 
     def to_dict(self):
         return {
             "vessel_id": self.vessel_id,
             "amount": self.amount,
-            "payment_date": str(self.payment_date),
             "payment_type": self.payment_type,
             "tax_amount": self.tax_amount,
+            "payment_date": str(self.payment_date),
         }
