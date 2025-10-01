06e851ca604f0cafc77dad745630da95d29fe6e2
SIDDI-NEHA-SREE <siddinehasree@gmail.com>
2025-10-01 16:06:23 +0530

    Update payments_service.py

--- MarinaManageentSystem/src/services/payments_service.py
+++ MarinaManageentSystem/src/services/payments_service.py
@@ -1,9 +1,9 @@
-from src.dao.payments_dao import PaymentsDAO
+from src.dao.base_dao import BaseDAO
 from src.models.payment import Payment
 
 class PaymentsService:
     def __init__(self):
-        self.dao = PaymentsDAO()
+        self.dao = BaseDAO("mmspayments", "payment_id")
 
     def record_payment(self, payment: Payment):
         return self.dao.insert(payment.to_dict())
