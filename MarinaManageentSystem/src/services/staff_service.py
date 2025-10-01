73ccb5fe48e57444d5c0bfdb0b094d9ff526edae
SIDDI-NEHA-SREE <siddinehasree@gmail.com>
2025-10-01 16:07:13 +0530

    Update staff_service.py

--- MarinaManageentSystem/src/services/staff_service.py
+++ MarinaManageentSystem/src/services/staff_service.py
@@ -1,12 +1,18 @@
-from src.dao.staff_dao import StaffDAO
+from src.dao.base_dao import BaseDAO
 from src.models.staff import Staff
 
 class StaffService:
     def __init__(self):
-        self.dao = StaffDAO()
+        self.dao = BaseDAO("mmsstaff", "staff_id")
 
     def add_staff(self, staff: Staff):
         return self.dao.insert(staff.to_dict())
 
     def list_staff(self):
         return self.dao.select().data
+
+    def update_staff(self, staff_id, updated_data):
+        return self.dao.update(staff_id, updated_data)
+
+    def delete_staff(self, staff_id):
+        return self.dao.delete(staff_id)
