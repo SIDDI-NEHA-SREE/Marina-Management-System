edea9413fcb9b900dc90a445a2155e666e5153f8
SIDDI-NEHA-SREE <siddinehasree@gmail.com>
2025-10-01 16:41:50 +0530

    Update violation.py

--- MarinaManageentSystem/src/models/violation.py
+++ MarinaManageentSystem/src/models/violation.py
@@ -1,13 +1,11 @@
 from datetime import date
 
 class Violation:
-    def __init__(self, vessel_id, violation_type, details, violation_id=None, date_reported=None, resolved_status="pending"):
-        self.violation_id = violation_id
+    def __init__(self, vessel_id, violation_type, details):
         self.vessel_id = vessel_id
         self.violation_type = violation_type
         self.details = details
-        self.date_reported = date_reported or date.today()
-        self.resolved_status = resolved_status
+        self.date_reported = date.today()
 
     def to_dict(self):
         return {
@@ -15,5 +13,4 @@ class Violation:
             "violation_type": self.violation_type,
             "details": self.details,
             "date_reported": str(self.date_reported),
-            "resolved_status": self.resolved_status,
         }
