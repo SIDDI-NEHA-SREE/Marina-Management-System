a118a4c7fe1987180a1f906a0af26d66618efee0
SIDDI-NEHA-SREE <siddinehasree@gmail.com>
2025-10-01 16:43:45 +0530

    Update dockings_service.py

--- MarinaManageentSystem/src/services/dockings_service.py
+++ MarinaManageentSystem/src/services/dockings_service.py
@@ -1,18 +1,17 @@
 from src.dao.base_dao import BaseDAO
-from src.models.docking import Docking
 
 class DockingsService:
     def __init__(self):
-        self.dao = BaseDAO("mmsdockings", "docking_id")
+        self.dao = BaseDAO("mmsdockings")
 
-    def dock_vessel(self, docking: Docking):
+    def dock_vessel(self, docking):
         return self.dao.insert(docking.to_dict())
 
     def list_dockings(self):
-        return self.dao.select().data
+        return self.dao.get_all()
 
-    def update_docking(self, docking_id, updated_data):
-        return self.dao.update(docking_id, updated_data)
+    def update_docking(self, docking_id, data):
+        return self.dao.update(docking_id, data, id_field="docking_id")
 
     def delete_docking(self, docking_id):
-        return self.dao.delete(docking_id)
+        return self.dao.delete(docking_id, id_field="docking_id")
