8d5c0715ca1aa87fbd9427a46f835479a3da0c43
SIDDI-NEHA-SREE <siddinehasree@gmail.com>
2025-10-01 15:32:04 +0530

    Update owners_service.py

--- MarinaManageentSystem/src/services/owners_service.py
+++ MarinaManageentSystem/src/services/owners_service.py
@@ -1,12 +1,27 @@
-from src.dao.owners_dao import OwnersDAO
+from src.utils.supabase_client import supabase
 from src.models.owner import Owner
 
 class OwnersService:
     def __init__(self):
-        self.dao = OwnersDAO()
+        self.client = supabase
+        self.table = "MMSOwners"
 
     def create_owner(self, owner: Owner):
-        return self.dao.insert(owner.to_dict())
+        """Insert a new owner into the database"""
+        res = self.client.table(self.table).insert(owner.to_dict()).execute()
+        return res.data
 
     def list_owners(self):
-        return self.dao.select().data
+        """Fetch all owners"""
+        res = self.client.table(self.table).select("*").execute()
+        return res.data
+
+    def update_owner(self, owner_id: int, updated_data: dict):
+        """Update owner details"""
+        res = self.client.table(self.table).update(updated_data).eq("owner_id", owner_id).execute()
+        return res.data
+
+    def delete_owner(self, owner_id: int):
+        """Delete owner by ID"""
+        res = self.client.table(self.table).delete().eq("owner_id", owner_id).execute()
+        return res.data
