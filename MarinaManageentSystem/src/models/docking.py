cf1505d4ce7b6a124fb78fc92febbcbb16abf018
SIDDI-NEHA-SREE <siddinehasree@gmail.com>
2025-10-01 16:40:05 +0530

    Update docking.py

--- MarinaManageentSystem/src/models/docking.py
+++ MarinaManageentSystem/src/models/docking.py
@@ -1,21 +1,16 @@
-from datetime import datetime, timezone
+from datetime import datetime
 
 class Docking:
-    def __init__(self, vessel_id, dock_location, dock_capacity=1, status="available", arrival_time=None, departure_time=None, docking_id=None):
-        self.docking_id = docking_id
+    def __init__(self, vessel_id, dock_location, dock_capacity):
         self.vessel_id = vessel_id
         self.dock_location = dock_location
         self.dock_capacity = dock_capacity
-        self.status = status
-        self.arrival_time = arrival_time or datetime.now(timezone.utc)
-        self.departure_time = departure_time
+        self.arrival_time = datetime.utcnow()
 
     def to_dict(self):
         return {
             "vessel_id": self.vessel_id,
             "dock_location": self.dock_location,
             "dock_capacity": self.dock_capacity,
-            "status": self.status,
             "arrival_time": self.arrival_time.isoformat(),
-            "departure_time": self.departure_time.isoformat() if self.departure_time else None,
         }
