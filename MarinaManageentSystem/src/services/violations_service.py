92950b8de4d19757fcddb549b85fbfcdd1be6149
SIDDI-NEHA-SREE <siddinehasree@gmail.com>
2025-10-01 22:42:25 +0530

    Update violations_service.py

--- MarinaManageentSystem/src/services/violations_service.py
+++ MarinaManageentSystem/src/services/violations_service.py
@@ -1,12 +1,20 @@
 from src.services.base_service import BaseService
-from src.models.violation import Violation
+from src.models.vessel import Vessel
 
-class ViolationsService(BaseService):
+class VesselsService(BaseService):
     def __init__(self):
-        super().__init__("mmsviolations")
+        super().__init__("mmsvessels")
 
-    def report_violation(self, violation: Violation):
-        return self.insert(violation.to_dict())
+    def create_vessel(self, vessel: Vessel):
+        return self.insert(vessel.to_dict())
 
-    def list_violations(self):
+    def update_vessel(self, vessel_id, data: dict):
+        return self.update(vessel_id, data, "vessel_id")
+
+    def delete_vessel(self, vessel_id):
+        return self.delete(vessel_id, "vessel_id")
+
+    def list_vessels(self):
         return self.select_all()
+
+
