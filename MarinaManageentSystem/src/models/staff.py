07baf15117328f5b72073cc9eeacfbb1599f840c
SIDDI-NEHA-SREE <siddinehasree@gmail.com>
2025-10-03 15:55:42 +0530

    Update staff.py

--- MarinaManageentSystem/src/models/staff.py
+++ MarinaManageentSystem/src/models/staff.py
@@ -1,18 +1,16 @@
-
 class Staff:
-    def __init__(self, name, role, contact_info, password, staff_id=None):
+    def __init__(self, name, role=None, contact_info=None, staff_id=None):
         self.staff_id = staff_id
         self.name = name
         self.role = role
         self.contact_info = contact_info
-        self.password = password
 
     def to_dict(self):
-        return {
-            "staff_id": self.staff_id,
+        data = {
             "name": self.name,
             "role": self.role,
             "contact_info": self.contact_info,
-            "password": self.password
         }
-
+        if self.staff_id is not None:
+            data["staff_id"] = self.staff_id
+        return data
